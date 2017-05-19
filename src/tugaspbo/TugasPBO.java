4faddba7972647e0abbee3a51cfe2c25a9628b1e
Fiki L <Fiki L@DESKTOP-5OC1DOK>
2017-05-19 15:10:16 +0700

    new commit

--- src/tugaspbo/TugasPBO.java
+++ src/tugaspbo/TugasPBO.java
@@ -1,5 +1,27 @@
 package tugaspbo;
 
+//mengimport library
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TugasPBO {
+    public static void main(String[] args) {
+        //buat object ArrayList, HashMap, Scanner
+        ArrayList<Sosmed> list = new ArrayList<>();
+        ArrayList<Sosmed> list2 = new ArrayList<>();
+        HashMap<String, ArrayList> dataHash = new HashMap<String, ArrayList>();
+        Scanner input = new Scanner(System.in);
+        
+        //membuat tipe data
+        String nama, email, jenisKelamin;
+        int mengulang;
+        int group;
+        
+        System.out.println("------------------------------------");
+        System.out.println("            SOSIAL MEDIA            ");
+        System.out.println("------------------------------------");
+        
+        //membuat try catch
+    }
 }
