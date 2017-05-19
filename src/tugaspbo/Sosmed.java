00b6a509512d07e4e438202a9f922875605ac247
ACER <ACER@LAPTOP-7KVC349G>
2017-05-19 14:30:46 +0700

    new commit

---
+++ src/tugaspbo/Sosmed.java
@@ -0,0 +1,34 @@
+package tugaspbo;
+
+
+public class Sosmed {
+    
+    private String nama;
+    private String email;
+    private String jenisKelamin;
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jeniskelamin) {
+        this.jenisKelamin = jeniskelamin;
+    }
+    
+}
